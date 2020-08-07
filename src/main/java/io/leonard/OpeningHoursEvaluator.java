5d69c7626ba32f86f558adf7bd931c2fcd15f1c5
Leonard Ehrenfried <mail@leonard.io>
2020-08-07 11:09:14 +0200

    Add initial evalution logic and tests

--- src/main/java/io/leonard/OpeningHoursEvaluator.java
+++ src/main/java/io/leonard/OpeningHoursEvaluator.java
@@ -1,13 +1,43 @@
 package io.leonard;
 
 import ch.poole.openinghoursparser.Rule;
-
+import ch.poole.openinghoursparser.TimeSpan;
+import ch.poole.openinghoursparser.WeekDayRange;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Collections;
 import java.util.List;
 
 public class OpeningHoursEvaluator {
 
     public static boolean isOpenAt(LocalDateTime time, List<Rule> rules) {
-        return true;
+        return rules.stream().anyMatch(rule -> rule.isTwentyfourseven() || isOpenAt(time, rule));
+    }
+
+    private static boolean isOpenAt(LocalDateTime time, Rule rule) {
+        return nullToEmptyList(rule.getDays()).stream()
+                        .anyMatch(dayRange -> isOpenAtDay(time, dayRange))
+                && nullToEmptyList(rule.getTimes()).stream()
+                        .anyMatch(timeSpan -> isOpenAtTime(time, timeSpan));
+    }
+
+    private static boolean isOpenAtDay(LocalDateTime time, WeekDayRange range) {
+        int ordinal = time.getDayOfWeek().ordinal();
+        return range.getStartDay().ordinal() <= ordinal && range.getEndDay().ordinal() >= ordinal;
+    }
+
+    private static boolean isOpenAtTime(LocalDateTime time, TimeSpan timeSpan) {
+        var minutesAfterMidnight = minutesAfterMidnight(time.toLocalTime());
+        return timeSpan.getStart() <= minutesAfterMidnight
+                && timeSpan.getEnd() >= minutesAfterMidnight;
+    }
+
+    private static int minutesAfterMidnight(LocalTime time) {
+        return time.getHour() * 60 + time.getMinute();
+    }
+
+    private static <T> List<T> nullToEmptyList(List<T> list) {
+        if (list == null) return Collections.emptyList();
+        else return list;
     }
 }
--- src/test/java/io/leonard/OpeningHoursEvaluatorTest.java
+++ src/test/java/io/leonard/OpeningHoursEvaluatorTest.java
@@ -1,15 +1,17 @@
 package io.leonard;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import ch.poole.openinghoursparser.OpeningHoursParseException;
 import ch.poole.openinghoursparser.OpeningHoursParser;
 import ch.poole.openinghoursparser.Rule;
-import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayInputStream;
 import java.time.LocalDateTime;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class OpeningHoursEvaluatorTest {
 
@@ -19,6 +21,22 @@ public class OpeningHoursEvaluatorTest {
         var rules = parseOpeningHours(openingHours);
         var time = LocalDateTime.parse("2020-08-06T14:28:04");
         assertTrue(OpeningHoursEvaluator.isOpenAt(time, rules));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/open.csv", delimiter = ',')
+    void shouldEvaluateAsOpen(String time, String openingHours) throws OpeningHoursParseException {
+        var rules = parseOpeningHours(openingHours);
+        var parsed = LocalDateTime.parse(time);
+        assertTrue(OpeningHoursEvaluator.isOpenAt(parsed, rules));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/closed.csv", delimiter = ',')
+    void shouldEvaluateAsClose(String time, String openingHours) throws OpeningHoursParseException {
+        var rules = parseOpeningHours(openingHours);
+        var parsed = LocalDateTime.parse(time);
+        assertFalse(OpeningHoursEvaluator.isOpenAt(parsed, rules));
     }
 
     private List<Rule> parseOpeningHours(String openingHours) throws OpeningHoursParseException {
